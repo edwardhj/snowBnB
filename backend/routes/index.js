803fe782b397e3d52ecc56b9dcf14d7b859e050b
Edward Jung <edwardjung2018@gmail.com>
2023-11-13 20:09:29 -0800

    "Add error-handling middleware for common errors and debugging"

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const apiRouter = require('./api');
 
-router.use('/api', apiRouter);
-
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
     const csrfToken = req.csrfToken();
@@ -12,5 +10,10 @@ router.get("/api/csrf/restore", (req, res) => {
       'XSRF-Token': csrfToken
     });
   });
+
+router.use('/api', apiRouter);
+
+
+
 
 module.exports = router;
