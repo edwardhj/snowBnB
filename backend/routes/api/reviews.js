c825632e9acfbb981bfb8bbb5d477989601c8dff
Edward Jung <edwardjung2018@gmail.com>
2023-12-05 16:36:55 -0800

    edit a review

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -92,6 +92,53 @@ router.post('/:reviewId/images', requireAuth, async (req, res) => {
     res.json(resImg);
 });
 
+// Edit a Review
+router.put('/:reviewId', requireAuth, async (req, res) => {
+    const { reviewId } = req.params;
+    const id = parseInt(reviewId, 10);
+    const userId = req.user.id;
+    const { review, stars } = req.body;
+
+    const rev = await Review.findByPk(id);
+    
+    // if review doesn't exist with specified id
+    if (!rev){
+        const err = new Error("Review couldn't be found");
+        err.status = 404;
+        throw err;
+    };
+    // if spot's owner doesn't match the logged in user
+    if (rev.userId !== userId){
+        const err = new Error('Forbidden');
+        err.status = 403;
+        throw err;
+    };
+
+    // check for bad update requests (invalid params)
+    if (review || stars){
+        const err = new Error('Bad Request');
+        err.errors = {};
+        err.status = 400;
+
+        if (review === undefined) err.errors.review = 'Review text is required';
+        if (stars && ![1, 2, 3, 4, 5].includes(stars)) err.errors.stars = 'Stars must be an integer from 1 to 5';
+        if (Object.keys(err.errors).length > 0) throw err;
+    };
+
+    // successful response
+    if (review) await rev.update({review: review});
+    if (stars) await rev.update({stars: stars});
+
+    res.json({
+        id: rev.id,
+        userId: rev.userId,
+        spotId: rev.spotId,
+        review: rev.review,
+        stars: rev.stars,
+        createdAt: rev.createdAt,
+        updatedAt: rev.updatedAt
+    });
+});
 
 // Delete a Spot
 router.delete('/:reviewId', requireAuth, async (req, res) => {
