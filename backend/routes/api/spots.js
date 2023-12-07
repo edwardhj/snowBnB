49d21fd48c58b93504097e4cf7145552d4b27943
Edward Jung <edwardjung2018@gmail.com>
2023-12-06 23:19:17 -0800

    updated edit a spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -26,6 +26,7 @@ const spotValidationErrors = [
     check('lng')
       .exists({ checkFalsy: true })
       .notEmpty()
+      .isFloat({min: -180, max: 180})
       .withMessage('Longitude must be within -180 and 180'),
     check('name')
       .exists({ checkFalsy: true })
@@ -35,6 +36,52 @@ const spotValidationErrors = [
       .exists({ checkFalsy: true})
       .withMessage('Description is required'),
     check('price')
+      .exists({ checkFalsy: true })
+      .isFloat({min:0.01})
+      .withMessage('Price per day must be a positive number'),  
+    handleValidationErrors
+];
+
+const spotEditValidationErrors = [
+    check('address')
+      .optional()
+      .exists({ checkFalsy: true })
+      .withMessage('Street address is required'),
+    check('city')
+      .optional()
+      .exists({ checkFalsy: true })
+      .withMessage('City is required'),
+    check('state')
+      .optional()
+      .exists({ checkFalsy: true })
+      .withMessage('State is required'),
+    check('country')
+      .optional()
+      .exists({ checkFalsy: true })
+      .withMessage('Country is required'),
+    check('lat')
+      .optional()
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isFloat({min: -90, max: 90})
+      .withMessage('Latitude must be within -90 and 90'),
+    check('lng')
+      .optional()
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .isFloat({min: -180, max: 180})
+      .withMessage('Longitude must be within -180 and 180'),
+    check('name')
+      .optional()
+      .exists({ checkFalsy: true })
+      .isLength({max: 50})
+      .withMessage('Name must be less than 50 characters'),
+    check('description')
+      .optional()
+      .exists({ checkFalsy: true})
+      .withMessage('Description is required'),
+    check('price')
+      .optional()
       .exists({ checkFalsy: true })
       .isFloat({min:0.01})
       .withMessage('Price per day must be a positive number'),  
@@ -455,7 +502,7 @@ router.post('/:spotId/bookings', requireAuth, bookingValidationErrors, async (re
 });
 
 // Edit a Spot
-router.put('/:spotId', requireAuth, spotValidationErrors, async (req, res) => {
+router.put('/:spotId', requireAuth, spotEditValidationErrors, async (req, res) => {
     const { spotId } = req.params;
     const id = parseInt(spotId, 10);
     const userId = req.user.id;
@@ -476,32 +523,30 @@ router.put('/:spotId', requireAuth, spotValidationErrors, async (req, res) => {
         throw err;
     };
 
-    const updatedSpot = await spot.update({
-        address: address,
-        city: city,
-        state: state,
-        country: country,
-        lat: lat,
-        lng: lng,
-        name: name,
-        description: description,
-        price: price
-    });
+    if (address) await spot.update(address);
+    if (city) await spot.update(city);
+    if (state) await spot.update(state);
+    if (country) await spot.update(country);
+    if (lat) await spot.update(lat);
+    if (lng) await spot.update(lng);
+    if (name) await spot.update(name);
+    if (description) await spot.update(description);
+    if (price) await spot.update(price);
 
     const resSpot = {
-        id: updatedSpot.id,
-        ownerId: updatedSpot.ownerId,
-        address: updatedSpot.address,
-        city: updatedSpot.city,
-        state: updatedSpot.state,
-        country: updatedSpot.country,
-        lat: updatedSpot.lat,
-        lng: updatedSpot.lng,
-        name: updatedSpot.name,
-        description: updatedSpot.description,
-        price: updatedSpot.price,
-        createdAt: updatedSpot.createdAt,
-        updatedAt: updatedSpot.updatedAt
+        id: spot.id,
+        ownerId: spot.ownerId,
+        address: spot.address,
+        city: spot.city,
+        state: spot.state,
+        country: spot.country,
+        lat: spot.lat,
+        lng: spot.lng,
+        name: spot.name,
+        description: spot.description,
+        price: spot.price,
+        createdAt: spot.createdAt,
+        updatedAt: spot.updatedAt
     };
 
     res.json(resSpot);
