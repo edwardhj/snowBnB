542753d453de8ac9275c87356c2f204ef838f25d
Edward Jung <edwardjung2018@gmail.com>
2024-01-13 12:26:59 -0800

    session reducer and login

---
+++ frontend/src/store/session.js
@@ -0,0 +1,49 @@
+import { csrfFetch } from "./csrf";
+const LOG_IN = 'session/LOG_IN';
+const LOG_OUT = 'session/LOG_OUT';
+
+const logIn = user => ({
+    type: LOG_IN,
+    user
+});
+
+const logOut = () => ({
+    type: LOG_OUT
+});
+
+export const login = (user) => async dispatch => {
+    const {credential, password} = user;
+    const response = await csrfFetch('/api/session', {
+        method: 'POST',
+        // headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({credential, password})
+    });
+    const data = await response.json();
+    dispatch(logIn(data.user));
+    return data;
+};
+
+// export const logout = () => async dispatch => {
+//     const response = await
+// }
+
+const initialState = {};
+
+const sessionReducer = (state = initialState, action) => {
+    switch(action.type){
+        case LOG_IN:
+            return {
+                ...state,
+                user: action.user
+            };
+        case LOG_OUT:
+            return {
+                ...state,
+                user: null
+            };
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
