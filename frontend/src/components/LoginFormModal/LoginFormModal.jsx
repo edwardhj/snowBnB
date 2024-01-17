2dc5a8e156bc035d06fd079a70ed2b5348a54478
Edward Jung <edwardjung2018@gmail.com>
2024-01-16 20:21:41 -0800

    modal login

---
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -0,0 +1,59 @@
+import { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
+
+function LoginFormModal() {
+  const dispatch = useDispatch();
+  const [credential, setCredential] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState({});
+  const { closeModal } = useModal();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors({});
+    return dispatch(sessionActions.login({ credential, password }))
+      .then(closeModal)
+      .catch(async (res) => {
+        const data = await res.json();
+        console.log(data)
+        if (data && data.message) {
+          setErrors(data);
+        }
+      });
+  };
+
+  return (
+    <>
+      <h1>Log In</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username or Email
+          <input
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        {errors.message && (
+          <p className='error'>{errors.message}</p>
+        )}
+        <button type="submit">Log In</button>
+      </form>
+    </>
+  );
+}
+
+export default LoginFormModal;
