eea6466508d24feadd501e4f0dcf59d693a81955
Edward Jung <edwardjung2018@gmail.com>
2024-01-25 22:23:18 -0800

    updated seeders

--- backend/db/seeders/20231129044137-snowboarding-bnbs-images.js
+++ backend/db/seeders/20231129044137-snowboarding-bnbs-images.js
@@ -78,7 +78,7 @@ module.exports = {
     options.tableName = 'SpotImages';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      spotId: { [Op.in]: [1, 2] }
+      spotId: { [Op.in]: [1, 2, 3, 4] }
     }, {});
   }
 };
